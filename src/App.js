befe9c9db726547f48a51012664405cec95d8631
laviedegeorge <laviedegeorge1292@gmail.com>
2020-07-11 00:03:55 +0100

    set up project environment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
